d257359359d7bf67233bef02f1a338d2cb4bc3c3
karina heras <karinaheras@MacBook-Pro-de-karina.local>
2020-06-23 22:02:08 +0200

    login

---
+++ src/assets/PostComponent.vue
@@ -0,0 +1,9 @@
+<template>
+<h2>{{post}}</h2>
+</template>
+<script>
+export default {
+ props:['post']
+}
+
+</script>
---
+++ src/assets/login/auth.vue
@@ -0,0 +1,20 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const  URL = 'http://localhost:3000';
+export default {
+  setUserLogged(userLogged) {
+    Cookies.set("userLogged", userLogged);
+  },
+  getUserLogged() {
+    return Cookies.get("userLogged");
+  },
+  register(username, password) {
+    const user = { username, password };
+    return axios.post(URL + "regiser", user);
+  },
+  login(username, password) {
+    const user = { username, password };
+    return axios.post(URL + "login", user);
+  }
+};
